4c4891849218a2a78fb7492c9bec0a98d673710c
S-Gabriela <betancourt_141162@students.pupr.edu>
2025-02-04 16:33:14 -0400

    Create fileA

---
+++ fileA.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void functionA() {
+    // Developer1: Change this message
+    cout << "Function A: Hello from Developer1!" << endl;
+
+    // Developer2: Add a simple sum calculation
+    int a = 5, b = 10, sum;
+	
+	
+    // Developer3: Modify this output
+    cout << "Sum: " << (a + b) << endl;
+}
+
+int main() {
+    functionA();
+    return 0;
+}
